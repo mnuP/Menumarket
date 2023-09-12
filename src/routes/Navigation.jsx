d1159212eb022cdb8d378d15548adccea034cd81
jorgeanayajoo <jorgeanaya94@hotmail.com>
2023-09-11 20:53:54 -0500

    se agrega navbar

---
+++ src/routes/Navigation.jsx
@@ -0,0 +1,48 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import "../style.css";
+
+
+function Navigation() {
+  return (
+    <>
+        <Navbar style={{backgroundColor:"#000000"}} key="md" expand="md" className="mb-3 text-dark">
+          <Container fluid>
+            <Navbar.Brand style={{width:"15vw", marginRight:"5vw"}} href="#">
+              <img
+                src="https://menumarket.co/wp-content/uploads/2022/03/menu-logo.png"
+                width="100%"
+                height="100%"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-md`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+              placement="end"
+            >
+              <Offcanvas.Body>
+                <Form className="d-flex">
+                  <Form.Control style={{width:"60vw"}}
+                    type="search"
+                    placeholder="Buscar Eventos"
+                    className="me-5"
+                    aria-label="Search"
+                    size="sm"
+                  />
+                  <Button style={{width:"15vw", backgroundColor:"#000000", borderBlockColor:"#CCD888", color:"#CCD888", borderWidth:"0.2em"}} className="ButtonName" variant="outline-success">Se Parte de Menumarket!</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+    </>
+  );
+}
+
+export default Navigation;
