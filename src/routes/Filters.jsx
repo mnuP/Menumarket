f1767d18ae48ecd2f1a8270530e3359dbc01539a
jorgeanayajoo <jorgeanaya94@hotmail.com>
2023-09-12 15:23:49 -0500

    cambios en css

--- src/routes/Filters.jsx
+++ src/routes/Filters.jsx
@@ -2,22 +2,22 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import style from"../navigation.module.css";
-import style from"../navigation.module.css";
+import style from"../filter.module.css";
 
 
 function Filters() {
   return (
     <Navbar className={style.navbar} >
-        <Container fluid>
+        <Container fluid className={style.dropdown}>
             {['Ciudad', 'Tipo', 'Precio', "Restaurantes", "Esperiencias Tematicas", "Regalos Corporativos", "Catas"].map(
             (variant) => (
 
                 <DropdownButton
                 key={variant}
+                id={`dropdown-split-variants-${variant}`}
+                variant={variant.toLowerCase()}
                 title={variant}
-                className={style.dropdown}
-                >     
+                >
                     <Dropdown.Item eventKey="1">Action</Dropdown.Item>
                     <Dropdown.Divider />
                     <Dropdown.Item eventKey="2">Another action</Dropdown.Item>          
