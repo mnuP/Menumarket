26bb85aab9db831c88fb12ffdea0b2e73322c766
jorgeanayajoo <jorgeanaya94@hotmail.com>
2023-09-18 18:42:44 -0500

    funcionamiiento de filtro de ciudades

--- src/routes/Items.jsx
+++ src/routes/Items.jsx
@@ -6,33 +6,71 @@ import { useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import style from"../filter.module.css";
-import Tipo from '../routes/Tipo';
-import Ciudad from '../routes/Ciudad';
-import BotonesFiltro from '../routes/BotonesFiltro';
-import Precio from '../routes/Precio';
+import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from "react";
+import { db } from '../firebase/firebase'
+import { onSnapshot, collection} from "firebase/firestore"; 
+
 
 function Items() {
-  const { items } = useFirestore('usersPrincipal');
+  const tipos = ["Restaurantes", "Esperiencias Tematicas", "Regalos Corporativos", "Catas"];
+  const ciudad = ["All", "Bogota", "Medellin"];
+  
+  const [items, setItems] = useState([]);
+  const [dishes, setDishes] = useState(items);
+  const collectionRef = collection(db, "usersPrincipal");
   const navigate = useNavigate();
-  let ciudadInformation;
 
-  function nameCiudad(ciudadData){
-    ciudadInformation = ciudadData.city;
+  const filterCity= (city) => {
+    let results;
+    if(city == "All"){
+      results = items
+    } else {
+      results = items.filter((curData) => {
+      return curData.city == city;
+    })}
+    setDishes(results)
   }
+
+  useEffect(()=> {
+      const dataTest =  onSnapshot(collectionRef,(querySnapshot) => { 
+          const items = [];
+          querySnapshot.forEach((doc) => {
+              items.push({...doc.data(), id: doc.id});
+          });
+      setItems(items);
+      setDishes(items);
+      });
+      return () => {
+          dataTest();
+      };
+  }, []);
 
   return (
     <>
-      <Navbar className={style.navbar} >
+      <Navbar className={style.navbar}>
         <Container fluid>
-          <Ciudad ciudad={nameCiudad}/>
-          <Tipo/>
-          <Precio/>
-          <BotonesFiltro/>            
+        {/* Ciudad */}
+          <select onChange={(e) => filterCity(e.target.value)} key="Ciudad" id={`dropdown-split-variants-ciudad`} title="Ciudad" variant = "ciudad">
+            {ciudad.map((item) => (
+              <>
+                <option eventKey={item} key={item}>{item}</option>            
+              </>
+            ))}
+          </select>
+          
+          {/* Tipo */}
+          {tipos.map((item) => (
+            <Button key={item} id={`dropdown-split-variants-${item}`} title={item} variant = {item}>
+              {item}
+            </Button>
+            ),
+          )}         
         </Container>
       </Navbar>
 
       <Row style={{margin: "0 3vw 3vw 3vw"}} xs={1} md={4} className="g-4">
-        {items.map((item) => (
+        {dishes.map((item) => (
           <div style={{padding:"0 2vw"}}>
           <Col>
             <Card key={item.id} onClick={()=>{navigate("ally", {state:{item}})}}>
