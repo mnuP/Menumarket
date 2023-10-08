317e7e2adda96818808832c675fcd192cb9cf318
jorgeanayajoo <jorgeanaya94@hotmail.com>
2023-10-08 16:57:25 -0500

    unos efectos en los items

--- src/routes/Items.jsx
+++ src/routes/Items.jsx
@@ -397,11 +397,11 @@ function Items({ query }) {
             </Container>
           </Navbar>
           
-          <Row style={{margin: "0 3vw 3vw 3vw"}} xs={1} md={4} className="g-4">
+          <Row xs={1} md={4} className="g-4 itemsPage">
             {dishes.map((item) => (
-                <div style={{padding:"0 2vw"}}>
+                <div className='ItemsInside'>
                   <Col>
-                    <Card key={item.id} onClick={()=>{navigate("ally", {state:{item}})}}>
+                    <Card className={"cards"} key={item.id} onClick={()=>{navigate("ally", {state:{item}})}}>
                       <Card.Img variant="top" src={item.photo} />
                       <Card.Body>
                         <Card.Title key={item.title}>{item.title}</Card.Title>
