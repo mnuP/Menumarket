fd84709ac973d7b673e1b61addaafd1a72c3a8c9
Manuel Paredes <mnu207@hotmail.com>
2023-11-23 16:37:53 -0500

    Different Desings

--- src/routes/allyView.jsx
+++ src/routes/allyView.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "../style.css";
 import imagenFondo from "../images/imagen-fondo-proveedor.png";
-import imagenSE from "../images/imagen-SE.png";
-import imagen1 from "../images/imagen-1.png";
+import imagenSE from "../images/logoRes.png";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import Footer from './Footer';
@@ -13,7 +12,7 @@ export const AllyView = (props) => {
   const navigate = useNavigate();
   const itemPass = location.state.item;
 
-  if(location.state.item.class == "Restaurantes"){
+  if(location.state.item.class === "Restaurantes"){
     return(<div>
       <div className="desktop">
         <div className="div">
@@ -57,7 +56,8 @@ export const AllyView = (props) => {
       </div>
       <Footer/>
     </div>);
-  }else if(location.state.item.class == "Cata"){
+    
+  }else if(location.state.item.class === "Cata"){
     return (<div>
       <div className="desktop">
         <div className="div">
@@ -73,25 +73,26 @@ export const AllyView = (props) => {
               </div>
             </div>
             <div className="informacion-basica">
-              <div className="frame">
-                <div className="text-wrapper-2">Capacidad: {location.state.item.capacity} Personas</div>
-              </div>
-              <div className="frame">
-                <div className="text-wrapper-2">Disponibilidad Horaria: {location.state.item.disponibility}</div>
-              </div>
-              <div className="frame">
-                <div className="text-wrapper-2">Duracion: {location.state.item.time}</div>
-              </div>
-              <div className="frame">
-                <div className="text-wrapper-2">{location.state.item.includes}</div>
-              </div>
+              <div className="text-wrapper-2" style={{fontSize: 18, lineHeight: 2, textAlign: "justify"}}>{location.state.item.description}</div>
             </div>
-            <img className="pepicons-pencil" alt="Pepicons pencil" src="https://api.iconify.design/pepicons-pencil/people.svg" />
-            <img className="octicon-person" alt="Octicon person" src="https://api.iconify.design/octicon/person-24.svg" />
-            <img className="formkit-time" alt="Formkit time" src="https://api.iconify.design/formkit/time.svg" />
-            <img className="codicon-settings" alt="Codicon settings" src="https://api.iconify.design/codicon/settings.svg" />
+            <img className="pepicons-pencil" alt="pepicon" src={imagenSE} />
           </div>
-          <p className="texto-experiencia">{location.state.item.description}</p>
+            <div className="texto-experiencia">
+              
+              <div className="text-wrapper-2">
+                <img className="iconTxt" alt="Pepicons pencil" src="https://api.iconify.design/pepicons-pencil/people.svg"/>
+                Capacidad: {location.state.item.capacity} Personas</div><br/>
+              <div className="text-wrapper-2">
+                <img className="iconTxt" alt="Octicon person" src="https://api.iconify.design/octicon/person-24.svg" />
+                Disponibilidad Horaria: {location.state.item.disponibility}</div><br/>
+              <div className="text-wrapper-2">
+              <img className="iconTxt" alt="Formkit time" src="https://api.iconify.design/formkit/time.svg" />
+                Duracion: {location.state.item.time}</div><br/>
+              <div className="text-wrapper-2">
+                <img className="iconTxt" alt="Codicon settings" src="https://api.iconify.design/codicon/settings.svg" />
+                {location.state.item.includes}</div>
+            </div>
+
           <div className="overlap">
             <img className="imagen-SE" alt="Imagen SE" src={location.state.item.photo2} />
             <div className="rectangle-2" />
@@ -129,7 +130,7 @@ export const AllyView = (props) => {
               <div className="text-wrapper-2">Duracion: {location.state.item.time}</div>
             </div>
             <div className="frame">
-              <div className="text-wrapper-2">{location.state.item.includes}</div>
+              <div className="text-wrapper-2">{location.state.item.includes} </div>
             </div>
           </div>
           <img className="pepicons-pencil" alt="Pepicons pencil" src="https://api.iconify.design/pepicons-pencil/people.svg" />
